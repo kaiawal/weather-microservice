5cca050141cbc3fd2ea763cc52c641a313e45d4d
Wiley Jones <jonewile@oregonstate.edu>
2026-02-16 15:25:40 -0800

    Started current weather with post parameters lat, long, city, state and zipcode

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import cors from "cors";
+
+const PORT = 3000;
+const API_KEY = "cc5a9c0a68979c8bb073f2a03c3041b7"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+});
+
+app.post("/", async (req, res) => {
+    let { lat, long, city, state, zipcode } = req.body;
+
+    if ((!lat || !long) && !city && !zipcode){
+        res.status(400).send("Not enough information provided")
+    }
+    // CITY ONLY 
+    else if ( (!lat || !long) && city && state) {
+        try {
+            const query = encodeURIComponent(`${city},${state},US`)            
+            const response = await fetch(
+                `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${API_KEY}`
+            )
+            const location = await response.json();
+            lat = location[0].lat;
+            long = location[0].lon;
+        }
+        catch (err) {
+                console.error("Failed to fetch location")
+        }
+    }
+
+    // ZIPCODE ONLY 
+    else if ((!lat || !long) && !city && !state) {
+        try {
+            const response = await fetch(
+                `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&appid=${API_KEY}`
+            )
+            const location = await response.json();
+            lat = location.lat;
+            long = location.lon;
+        }
+        catch (err) {
+            console.error("Failed to fetch location")
+        }
+    }
+
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&appid=${API_KEY}`
+        )
+        const weather = await response.json();
+
+        return res.status(200).send(weather);
+        }
+    catch (err) {
+        console.error("Failed to fetch weather")
+    }
+});
